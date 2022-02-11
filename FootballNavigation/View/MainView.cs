3cee2759b5fe378592d0374a6bfaf2b252f60960
Николай Стоянов <ns08059222@edu.mon.bg>
2022-02-11 12:16:51 +0200

    Initialize project

---
+++ FootballNavigation/View/MainView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FootballNavigation.View
+{
+    public partial class MainView : Form
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnField_Click(object sender, EventArgs e)
+        {
+            FieldView fieldView = new FieldView();
+            fieldView.Show();
+            this.Hide();
+        }
+
+        private void btnManager_Click(object sender, EventArgs e)
+        {
+            ManagerView managerView = new ManagerView();
+            managerView.Show();
+            this.Hide();
+        }
+    }
+}
